7baa3bb8c590d5750fc361da36c4fa90e5008057
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2023-04-04 00:22:20 +0600

    component plan added

--- restro-app/src/App.js
+++ restro-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainComponent from './MainComponent/MainComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <MainComponent></MainComponent>
     </div>
   );
 }
